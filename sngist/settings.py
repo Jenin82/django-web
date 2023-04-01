5bdf2f2534e482ea795a9e17bb4f7b50e4d1fe8e
Jenin82 <jenin8282@gmail.com>
2023-04-01 17:45:12 +0530

    t

--- sngist/settings.py
+++ sngist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wo6p1bt&-ocyf(sz(zlip--w!a6vid2g8@7)4##z0&v90pn-^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.up.railway.app/','127.0.0.1:8000/']
+ALLOWED_HOSTS = ['sngist.up.railway.app/','127.0.0.1:8000/']
 
 
 # Application definition
