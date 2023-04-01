0eda82c7476a438ca746956b499715d63cd63e59
Jenin82 <jenin8282@gmail.com>
2023-04-01 19:33:05 +0530

    creating new grievance by user

--- grievance/views.py
+++ grievance/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -14,3 +15,13 @@ def room(request, pk):
   room = Room.objects.get(id=pk)
   context = {'room': room}
   return render(request, "grievance/room.html", context)
+
+def createRoom(request):
+  form = RoomForm()
+  context = {'form': form}
+  if request.method == 'POST':
+    form = RoomForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  return render(request, "grievance/room_form.html", context)
