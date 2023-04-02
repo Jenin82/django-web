0fa7f6f708d7b6af25078a4f104cd33be58907e7
Jenin82 <jenin8282@gmail.com>
2023-04-02 18:32:57 +0530

    grievance description

--- grievance/views.py
+++ grievance/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def loginPage(request):
+  if request.user.is_authenticated:
+    return redirect('home')
   if request.method == "POST":
     username = request.POST["username"]
     password = request.POST["password"]
