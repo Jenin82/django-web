a257f4e106dde12ed5528478b8182ceb72948b21
Jenin82 <jenin8282@gmail.com>
2023-04-04 09:26:41 +0530

    s

--- grievance/urls.py
+++ grievance/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 		path('login/', views.loginPage, name="login"),
 		path('logout/', views.logoutPage, name="logout"),
-    path('', views.home, name="home"),
+    path('', views.home, name="g-home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('create-room/', views.createRoom, name="create-room"),
