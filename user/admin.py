60c60fc5d51af3c3996d93a4629d445f4f88ab08
Jenin82 <jenin8282@gmail.com>
2023-04-26 22:03:51 +0530

    admin panel setup

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
 
-# Register your models here.
+from typing import Set
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+	def get_form(self, request, obj=None, **kwargs):
+		form = super().get_form(request, obj, **kwargs)
+		is_superuser = request.user.is_superuser
+		disabled_fields = set()  # type: Set[str]
+
+		if not is_superuser:
+				disabled_fields |= {
+						'username',
+						'is_superuser',
+						'user_permissions',
+				}
+
+			# Prevent non-superusers from editing their own permissions
+		if (
+				not is_superuser
+				and obj is not None
+				and obj == request.user
+		):
+				disabled_fields |= {
+						'is_staff',
+						'is_superuser',
+						'groups',
+						'user_permissions',
+				}
+
+		for f in disabled_fields:
+				if f in form.base_fields:
+						form.base_fields[f].disabled = True
+
+		return form
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
