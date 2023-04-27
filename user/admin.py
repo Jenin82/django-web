41d2529ef4f9082974f68a9b3e51c04822fcf4c4
Jenin82 <jenin8282@gmail.com>
2023-04-27 07:11:51 +0530

    admin perms fix

--- user/admin.py
+++ user/admin.py
@@ -5,19 +5,29 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
+def is_member(user):
+    return user.is_superuser or user.groups.filter(name='admin_cse').exists()
+
 class CustomUserAdmin(UserAdmin):
 	def get_form(self, request, obj=None, **kwargs):
 		form = super().get_form(request, obj, **kwargs)
 		is_superuser = request.user.is_superuser
 		disabled_fields = set()  # type: Set[str]
-
-		if not is_superuser:
+  
+		if not is_member(request.user):
 				disabled_fields |= {
-						'username',
+						'is_superuser',
+						'groups',
+						'user_permissions',
+				}
+    
+		if is_member(request.user):
+				disabled_fields |= {
 						'is_superuser',
 						'user_permissions',
 				}
-
+    
+    
 			# Prevent non-superusers from editing their own permissions
 		if (
 				not is_superuser
